a4b327a7dba03bde7b22e7454d63d9b96fc9450d
Urmil Bhansali <urmilbhansali@Mac.attlocal.net>
2025-11-30 21:50:34 -0600

    Fix server to listen on 0.0.0.0 for Railway deployment

--- Shah Website/server.js
+++ Shah Website/server.js
@@ -1235,6 +1235,6 @@ app.delete('/api/admin/pricelist/tiers/:tier/customers', requireAdmin, (req, res
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
